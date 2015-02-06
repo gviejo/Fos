40419070490c5a1dfa7979e3bdc10a5f743f5f72
Guillaume Viejo <guillaume.viejo@gmail.com>
2015-02-06 13:25:47 +0100

    one file

--- run/ccall/pi_call.cpp
+++ run/ccall/pi_call.cpp
@@ -4,9 +4,6 @@
 #include <sstream>
 #include <iterator>
 #include <math.h>
-#include "normal.cpp"
-using namespace std;
-
 
 // extern "C" {
 // 		int mvndst_(int *, double (*)[2], double (*)[2], int (*)[2], double *, int *,
@@ -25,6 +22,97 @@ using namespace std;
 // 	// double upper [2] = {(x+grain_-(-0.235))/sqrt(varGoal), (y+grain_-1.193)/sqrt(varGoal)};
 // 	return ND2(lower[0], lower[1], 0.0);	
 // }	
+double max(double a, double b) {
+	if (a>b)
+        return a;
+	else
+        return b;
+}
+double min(double a, double b) {
+	if (a>b)
+        return b;
+	else
+        return a;
+}
+double sgn(double a) {
+	if (a>0)
+        return 1.;
+	else if (a<0)
+        return -1.;
+	else
+        return 0.;
+}
+double ndf(double t) {
+	return 0.398942280401433*exp(-t*t/2);
+}
+double nc(double x) {
+	double result;
+	if (x<-7.)
+        result = ndf(x)/sqrt(1.+x*x);
+	else if (x>7.)
+        result = 1. - nc(-x);
+	else {
+		result = 0.2316419;
+		double a[5] = {0.31938153,-0.356563782,1.781477937,-1.821255978,1.330274429};
+		result=1./(1+result*fabs(x));
+		result=1-ndf(x)*(result*(a[0]+result*(a[1]+result*(a[2]+result*(a[3]+result*a[4])))));
+		if (x<=0.) result=1.-result;
+	}
+	return result;
+}
+double fxy(double x, double y, double a, double b, double rho) {
+    double a_s;
+    double b_s;
+	double result;
+    a_s = a / sqrt(2 * (1 - rho * rho));
+    b_s = b / sqrt(2 * (1 - rho * rho));
+    result = exp(a_s * (2 * x - a_s) + b_s * (2 * y - b_s) + 2 * rho * (x - a_s) * (y - b_s));
+	return result;
+}
+double Ntwo(double a, double b, double rho) {
+    double aij[4]={0.325303, 0.4211071, 0.1334425, 0.006374323};
+    double bij[4]={0.1337764, 0.6243247, 1.3425378,2.2626645};
+    int i;
+    int j;
+    double result;
+    result = 0;
+    for(i=0;i<=3;i++) {
+        for(j=0;j<=3;j++) {
+          	result+=aij[i] * aij[j] * fxy(bij[i], bij[j], a, b, rho); 
+        }
+    }
+    result = result * sqrt(1 - rho * rho) / M_PI;
+return result;
+}
+double ND2(double a, double b, double rho)
+{
+    double rho1;
+    double rho2;
+    double denominator;
+    double result;
+
+    if (a * b * rho <= 0) {
+        if (a <= 0 && b <= 0 && rho <= 0)
+            result = Ntwo(a, b, rho);
+        else if (a <= 0 && b * rho >= 0)
+            result = nc(a) - Ntwo(a, -b, -rho);
+        else if (b <= 0 && rho >= 0)
+            result = nc(b) - Ntwo(-a, b, -rho);
+        else
+            result = nc(a) + nc(b) - 1 + Ntwo(-a, -b, rho);
+    }
+    else
+            {
+        denominator = sqrt(a * a - 2 * rho * a * b + b * b);
+        rho1 = (rho * a - b) * sgn(a) / denominator;
+        rho2 = (rho * b - a) * sgn(b) / denominator;
+        result = ND2(a, 0, rho1) + ND2(b, 0, rho2) - (1 - sgn(a) * sgn(b)) / 4;
+    }
+    if (result < 0) result = 0;
+    return result;
+}
+
+
 double cdf_multi(double *lower, double *upper, double *mu, double cov) {
 	for (int i=0;i<2;i++) {
 		lower[i] = (lower[i]-mu[i])/sqrt(cov);
@@ -294,7 +382,7 @@ void sferes_call(double * fit, const int N, const char* data_dir, double beta_, 
 	double gamma=0.0+gamma_*(0.999999999-0.0);	
 	double eta=0.1+(0.1-0.01)*eta_;
 
-	// std::cout << beta << " " << gamma << " " << eta << std::endl;
+	std::cout << beta << " " << gamma << " " << eta << std::endl;
 	const int n_state = 3;
 	const int n_action = 4;
 	const int n_case = 30;
@@ -334,10 +422,10 @@ void sferes_call(double * fit, const int N, const char* data_dir, double beta_, 
 	fileinfo.append("info.txt");
 	filedata.append("possarpossible.txt");	
 	std::ifstream data_fileinfo(fileinfo.c_str());
-	string line;
+	std::string line;
 	if (data_fileinfo.is_open()) {
 		getline (data_fileinfo, line);
-		stringstream stream(line);
+		std::stringstream stream(line);
 		std::vector<int> values(
 			(std::istream_iterator<int>(stream)),
 			(std::istream_iterator<int>()));
@@ -356,7 +444,7 @@ void sferes_call(double * fit, const int N, const char* data_dir, double beta_, 
 		for (int i=0;i<nb_points;i++) 
 		{  			
 			getline (data_filedata,line);			
-			stringstream stream(line);
+			std::stringstream stream(line);
 			std::vector<int> values(
      			(std::istream_iterator<int>(stream)),
      			(std::istream_iterator<int>()));
--- run/sferes/sferes_pi_B137_52trials.py
+++ run/sferes/sferes_pi_B137_52trials.py
@@ -7,13 +7,14 @@ from Wrap import TYMaze
 from Models import PI
 import cPickle as pickle
 
-parameters = {'beta': 100.0,
- 'eta': 0.109,
- 'gamma': 0.1}
+parameters = {'beta': 13.2636,
+ 'eta': 0.138274,
+ 'gamma': 0.183356}
+   
 # print parameters
 model = PI(parameters)
 opt = TYMaze(model)
-with open('../../data/B137_52trials.pickle') as f:
+with open('../../data/B163_52trials.pickle') as f:
 	data = pickle.load(f)
 llh = opt.sferes(data)
-# print llh
+print llh
